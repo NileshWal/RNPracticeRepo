e72cf14f480f692fadfb1da7905ef0c8d5c422df
DESKTOP-GL1Q2HC\admin <nileshswbvl@gmail.com>
2025-11-16 19:24:09 +0530

    1. Tested the lifecycle methods from class component to functional component.

--- components/MyText.tsx
+++ components/MyText.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useState, useEffect } from 'react';
 import { Text } from 'react-native';
 import CustomLogs from '../components/CustomLogs';
 
@@ -12,66 +12,94 @@ interface MyTextState {
   fullName: string;
 }
 
-class MyText extends Component<MyTextProps, MyTextState> {
-  TAG = 'MyText';
+const MyText = (props: MyTextProps) => {
+  const TAG = 'MyText';
 
-  constructor(props: MyTextProps) {
-    super(props);
-    this.state = {
-      fullName: this.props.name + ' Walavalkar',
+  const [fullName, setFullName] = useState('');
+
+  useEffect(() => {
+    CustomLogs.debug(TAG, 'Our component has been mounted');
+    return () => {
+      CustomLogs.debug(TAG, 'Our component is now being destroyed');
     };
-  }
+  }, []);
 
-  componentDidMount() {
-    CustomLogs.debug(this.TAG, 'componentDidMount called');
-  }
+  useEffect(() => {
+    CustomLogs.debug(TAG, 'Value of fullName changed');
+  }, [fullName]);
 
-  shouldComponentUpdate(
-    nextProps: Readonly<MyTextProps>,
-    nextState: Readonly<MyTextState>,
-    nextContext: any,
-  ): boolean {
-    CustomLogs.debug(this.TAG, 'shouldComponentUpdate called');
-    return true;
-  }
-
-  getSnapshotBeforeUpdate(
-    prevProps: Readonly<MyTextProps>,
-    prevState: Readonly<MyTextState>,
-  ): any {
-    CustomLogs.debug(this.TAG, 'getSnapshotBeforeUpdate called');
-    return null;
-  }
-
-  componentDidUpdate(
-    prevProps: Readonly<MyTextProps>,
-    prevState: Readonly<MyTextState>,
-    snapshot?: any,
-  ): void {
-    CustomLogs.debug(
-      this.TAG,
-      `componentDidUpdate called. prevFullName=${prevState.fullName}, newFullName=${this.state.fullName}`,
-    );
-  }
-
-  componentWillUnmount(): void {
-    CustomLogs.debug(this.TAG, 'componentWillUnmount called');
-  }
-
-  handleTextClick = (): void => {
-    this.setState({ ...this.state, ...{ fullName: 'John Doe' } });
-  };
-
-  render() {
-    return (
-      <Text
-        style={{ color: 'green', backgroundColor: '#ffffff' }}
-        onPress={this.handleTextClick}
-      >
-        Hello! {this.props.name} React native world from {this.state.fullName}.
-      </Text>
-    );
-  }
-}
+  return (
+    <Text
+      style={{ color: 'green', backgroundColor: '#ffffff' }}
+      onPress={() => setFullName('Nilesh Here')}
+    >
+      Hello! {props.name} React native world from {fullName}.
+    </Text>
+  );
+};
 
 export default MyText;
+
+// class MyText extends Component<MyTextProps, MyTextState> {
+//   TAG = 'MyText';
+
+//   constructor(props: MyTextProps) {
+//     super(props);
+//     this.state = {
+//       fullName: this.props.name + ' Walavalkar',
+//     };
+//   }
+
+//   componentDidMount() {
+//     CustomLogs.debug(this.TAG, 'componentDidMount called');
+//   }
+
+//   shouldComponentUpdate(
+//     nextProps: Readonly<MyTextProps>,
+//     nextState: Readonly<MyTextState>,
+//     nextContext: any,
+//   ): boolean {
+//     CustomLogs.debug(this.TAG, 'shouldComponentUpdate called');
+//     return true;
+//   }
+
+//   getSnapshotBeforeUpdate(
+//     prevProps: Readonly<MyTextProps>,
+//     prevState: Readonly<MyTextState>,
+//   ): any {
+//     CustomLogs.debug(this.TAG, 'getSnapshotBeforeUpdate called');
+//     return null;
+//   }
+
+//   componentDidUpdate(
+//     prevProps: Readonly<MyTextProps>,
+//     prevState: Readonly<MyTextState>,
+//     snapshot?: any,
+//   ): void {
+//     CustomLogs.debug(
+//       this.TAG,
+//       `componentDidUpdate called. prevFullName=${prevState.fullName}, newFullName=${this.state.fullName}`,
+//     );
+//   }
+
+//   componentWillUnmount(): void {
+//     CustomLogs.debug(this.TAG, 'componentWillUnmount called');
+//   }
+
+//   handleTextClick = (): void => {
+//     this.setState({ ...this.state, ...{ fullName: 'John Doe' } });
+//   };
+
+//   render() {
+//     return (
+//       <Text
+//         style={{ color: 'green', backgroundColor: '#ffffff' }}
+//         onPress={this.handleTextClick}
+//       >
+//         Hello! {this.props.name} React native world from {this.state.fullName}.
+//       </Text>
+//     );
+//   }
+// }
+
+// export default MyText;
